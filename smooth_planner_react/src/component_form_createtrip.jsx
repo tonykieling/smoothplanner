1c441f7dfb6be2ac41db38051e0475db84633a80
sumedha <sumedhanarayanan@gmail.com>
2019-03-02 23:46:39 +0000

    using geo suggest to auto complete create new form

--- smooth_planner_react/src/component_form_createtrip.jsx
+++ smooth_planner_react/src/component_form_createtrip.jsx
@@ -1,24 +1,38 @@
 import React, { Component } from 'react';
+import Geosuggest from 'react-geosuggest';
 
 class CreateItinerary extends Component {
   render() {
     return (
-      <div className="create-itinerary">
-        <h3>Create New Itinerary</h3>
-        <form >
-          <div className="form-group">
-            <label htmlFor="itinerary-name">Itinerary Title</label>
-            <input type="text" name="itinerary-name" className="form-control" placeholder="Enter a title for your Itinerary"/>
-            <small id="emailHelp" className="form-text text-muted">Example: Canada Day 2019, Trip to India etc</small>
-          </div>
-          <label htmlFor="dt_start">Itinerary begins on: </label>
-          <input type ='date' name="dt_start" className="form-control"/>
-
-          <label htmlFor="dt_end">Itinerary end on: </label>
-          <input type ='date' name="dt_end" className="form-control"/>
-            <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
+      <div className="create-form-container">
+      <div className="form-title">
+      <h4 className="card-title">Create New Itinerary</h4>
       </div>
+      <form>
+        <div className="form-group">
+          <input type="text" name="itinerary-name" className="form-control" placeholder="Enter a title for your Itinerary"/>
+        </div>
+        <div className="form-group row">
+            <label for="dt_start" className="col-sm-1 col-form-label">Trip begins: </label>
+            <input type ='date' name="dt_start" className="form-control col-sm-11"/>
+          </div>
+          <div className="form-group row">
+            <label for="dt_end" className="col-sm-1 col-form-label">Trip ends on: </label>
+            <input type ='date' name="dt_end" className="form-control col-sm-11"/>
+          </div>  
+        <Geosuggest 
+          placeholder="Enter a destination for your trip like: Paris, Victoria, Australia etc."
+          className="form-group"
+          inputClassName="form-control"
+          suggestsClassName="list-group"
+          suggestItemClassName="list-group-item"
+          type="text"
+          />
+        <div className ="form-group">
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </div>
+      </form>
+    </div>
     )
   }
 }
