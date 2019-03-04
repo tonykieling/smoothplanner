cd6aad717ad0250f4ccd2323667d75c2acc3cca2
sumedha <sumedhanarayanan@gmail.com>
2019-03-04 22:09:39 +0000

    complete auto complete form in a separate component

--- smooth_planner_react/src/component_form_createtrip.jsx
+++ smooth_planner_react/src/component_form_createtrip.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Script from 'react-load-script';
+import LocationSearchInput from './component_form_autocomplete';
 
 class CreateItinerary extends Component {
   constructor(props) {
@@ -11,49 +12,11 @@ class CreateItinerary extends Component {
       title: 'New Trip',
     };
     const googleAPIURL = `https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_API_KEY}&libraries=places`;
-    //Bind Functions
-    this.handleScriptLoad = this.handleScriptLoad.bind(this);
-    this.handlePlaceSelect = this.handlePlaceSelect.bind(this);
-
   }
-  handleScriptLoad() { 
-    // Declare Options For Autocomplete 
-    // const options = { types: ["(cities)","(regions)"] }; 
-    
-    // Initialize Google Autocomplete 
-    /*global google*/
-    this.autocomplete = new google.maps.places.Autocomplete(
-                          document.getElementById('searchTextField')
-                        ); 
-    // Fire Event when a suggested name is selected
-    this.autocomplete.addListener("place_changed",
-                                  this.handlePlaceSelect); 
-  }
-  handlePlaceSelect() {
-
-    // Extract City From Address Object
-    let addressObject = this.autocomplete.getPlace();
-    let address = addressObject.address_components;
-
-    // Check if address is valid
-    if (address) {
-      // Set State
-      this.setState(
-        {
-          city: address[0].long_name,
-          query: addressObject.formatted_address,
-        }
-      );
-    }
-  }
-  on
-
+  
   render() {
     return (
       <div className="create-form-container">
-       <Script url = {this.googleAPIURL}
-      onLoad={this.handleScriptLoad}        
-    />
       <div className="form-title">
       <h4 className="card-title">Create New Itinerary</h4>
       </div>
@@ -62,20 +25,15 @@ class CreateItinerary extends Component {
           <input type="text" name="itinerary-name" className="form-control" placeholder="Enter a title for your Itinerary"/>
         </div>
         <div className="form-group row">
-            <label for="dt_start" className="col-sm-1 col-form-label">Trip begins: </label>
-            <input type ='date' name="dt_start" className="form-control col-sm-11"/>
+            <label for="dt_start" className="col-sm-6 col-form-label">Trip begins: </label>
+            <input type ='date' name="dt_start" className="form-control col-sm-6"/>
           </div>
           <div className="form-group row">
-            <label for="dt_end" className="col-sm-1 col-form-label">Trip ends on: </label>
-            <input type ='date' name="dt_end" className="form-control col-sm-11"/>
+            <label for="dt_end" className="col-sm-6 col-form-label">Trip ends on: </label>
+            <input type ='date' name="dt_end" className="form-control col-sm-6"/>
           </div>  
-        <div className="form-group row">
-        <input 
-          id="searchTextField" 
-          placeholder="Enter destination for your itinerary. Example: Canada, India, Paris, Banff etc." 
-          hintText="Search Destination" 
-          className="form-control"
-          value={this.state.query} />
+        <div className="form-group">
+        <LocationSearchInput />
         </div>
         <div className ="form-group">
           <button type="submit" className="btn btn-primary">Submit</button>
--- smooth_planner_react/src/component_recomendation.jsx
+++ smooth_planner_react/src/component_recomendation.jsx
@@ -6,7 +6,9 @@ class RecommendationItem extends Component {
   render() {
     return (
       <div>
-        <h4>{this.props.title}</h4>
+        <h4>Restaurant recommendations in Vancouver</h4>
+        <h5>{this.props.details.name}</h5>
+
       </div>
     )
   }
@@ -18,12 +20,11 @@ export default class GoogleRecomendation extends Component {
     const googleAPIURL = `https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_API_KEY}&libraries=places`;
     this.handleScriptLoad = this.handleScriptLoad.bind(this);
     this.state = {
-      recomendations: {name: ''}
+      recommendations: [],
     }
   }
   handleScriptLoad() {
-    
-    const vancouver = new google.maps.LatLng(49.2812035,-123.1170263,17);
+    const vancouver = new google.maps.LatLng(49.2812035,-123.1170263,17); // vancouver's lat and long.
     const map = new google.maps.Map(document.getElementById('map'), {
       center: vancouver,
       zoom: 15
@@ -31,24 +32,30 @@ export default class GoogleRecomendation extends Component {
     const service = new google.maps.places.PlacesService(map);
     const request = {
       location: vancouver,
-      radius: 10000,
+      radius: 10000, // radius in meters
       query: "restaurant"
     }
-    function callback(results, status) {
+    //  text search returns top 20 results for the query and location. For example, in this hardcoded query - it will return top 20 restaurants in vancouver in 1
+    service.textSearch(request,(results, status) => {
       console.log(status);
-      this.state.recomendations.name = results[0].name;
-    }
-    service.textSearch(request, callback);
+      console.log(results);
+      // results gives back the name, address, map position etc. For ratings, website etc. will have to call place details API like below -
+      let placeId = results[0].place_id;
+      let placeName = results[0].name;
+      // API call to Place details google API to get details
+      this.setState({recommendations: results[0]})
+    });
+    
 
   }
   render() {
     return (
       <div>
         <div id="map"></div>
-        <Script url = {this.googleAPIURL}   
+        <Script url = {this.googleAPIURL}
          onLoad = {this.handleScriptLoad}
         />
-        <RecommendationItem name={this.state.recomendations.name} />
+        <RecommendationItem details={this.state.recommendations} />
       </div>
     )
   }
