7c7f04166a6145eb358945fc231e827ab6dc5ce9
Tony Kieling <tony.kieling@gmail.com>
2019-03-06 04:05:10 +0000

    delete items is working

--- smooth_planner_api/app/controllers/api/v1/items_controller.rb
+++ smooth_planner_api/app/controllers/api/v1/items_controller.rb
@@ -2,7 +2,28 @@ module Api::V1
   class ItemsController < ApplicationController
     def index
       @items = Item.all
+      puts "HEREEEE"
       render json: @items
     end
+
+    def destroy
+      @item = Item.find(params[:id])
+      
+      if @item.destroy
+        head :no_content, status: :ok
+      else
+        render json: @item.errors, status: :unprocessable_entity
+      end
+
+    end
+
+
+
+    # private
+
+    # def item_params
+    #   params.require(:item).permit(:id)
+    # end
+
   end
 end
