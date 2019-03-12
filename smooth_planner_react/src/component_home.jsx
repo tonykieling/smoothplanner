c3100633386dec6cda52a45f3253c83ae4ee26ee
silentscribe <seelive.i@gmail.com>
2019-03-12 22:04:50 +0000

    arrow added to home page

--- smooth_planner_react/src/component_home.jsx
+++ smooth_planner_react/src/component_home.jsx
@@ -6,9 +6,8 @@ export default class Home extends Component {
       <div>
           <div className="home-card">
             <h1> Hi <b>{this.props.user}</b>!!</h1> <br />
-            <h1>Welcome to <b>Smooth Planner</b> App.</h1> <br /><br />
-            <h3>Now, you will be able to plan your trip</h3>
-            <h3> in a easy way.</h3>
+            <h1>Welcome to Smooth Planner</h1> <br /><br />
+            <h3><i className="far fa-arrow-alt-circle-left fa-2x"></i> To begin select or create new trip</h3>
           </div>        
       </div>
     )
